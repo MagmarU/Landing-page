ba1ff2f5e380d49c9f65b02fc4c947a58728fd7a
MagmarU <meneskuchno@mail.ru>
2023-03-14 16:56:38 +0300

    v.0.2

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let menu_button = document.body.querySelector('.navbar_menu_button');
 let popup = document.body.querySelector('.popup_menu');
 menu_button.addEventListener('click', function(event) {
     event.preventDefault();
-    console.log( popup );
     popup.hidden = false;
     setTimeout(() => popup.style.height = '100%', 0);
     document.body.style.overflow = 'hidden';
@@ -17,18 +16,20 @@ popup_button.addEventListener('click', function(event) {
 });
 
 let feedback_button = document.body.querySelectorAll('.feedback_button');
+let feedback_popup_flex = document.body.querySelector('.popup_feedback_flex');
 let feedback_popup = document.body.querySelector('.popup_feedback');
+
 for( let button of feedback_button ) {
     button.addEventListener('click', function(event) {
     event.preventDefault();
-    feedback_popup.hidden = false;
-    feedback_popup.style.left = ((window.innerWidth - feedback_popup.clientWidth) / 2) / window.innerWidth * 100 + '%';
-    feedback_popup.style.top = ((window.innerHeight - feedback_popup.clientHeight) / 2) / window.innerHeight * 100 + '%'
+    feedback_popup_flex.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
 });
 }
 
 let close_button = document.body.querySelector('.close_icon_image');
 
-close_button.addEventListener('click', function(event) {
-    feedback_popup.hidden = true;
+close_button.addEventListener('click', function() {
+    feedback_popup_flex.style.display = 'none';
+    document.body.style.overflow = 'visible';
 });
